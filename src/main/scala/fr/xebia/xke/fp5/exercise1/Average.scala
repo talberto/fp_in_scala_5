d9caeaf175fb581bace7c2e40fd3991ca96e96e4
Tomas Rodriguez <trodriguez@xebia.fr>
2016-03-01 15:02:55 +0100

    Exercice 1 step 2

--- src/main/scala/fr/xebia/xke/fp5/exercise1/Average.scala
+++ src/main/scala/fr/xebia/xke/fp5/exercise1/Average.scala
@@ -9,6 +9,19 @@ object OopAverage {
   /*
   Implémenter une fonction qui calcule l'average d'une liste d'entiers
    */
-  def avgInt(numbers: Int*): Int = ???
+  def avgInt(numbers: Int*): Int = numbers.reduce(_ + _) / numbers.length
 
+  /*
+  Implémenter une fonction qui calcule l'average d'une liste d'entiers
+   */
+  def avgDouble(numbers: Double*): Double = ???
+
+
+  /**
+    * Q: quelle est la différence entre les fonctions avgInt et avgDouble?
+    *
+    * Q: Peut-on faire une fonction générique qui traite les Int et les Double à la fois? Dit
+    * autrement, peut-on mutualiser le code des deux fonctions?
+    *
+    */
 }
