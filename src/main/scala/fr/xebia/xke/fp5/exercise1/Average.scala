a8e652f253fa3ce26253eaf5fc15ff21eddc7bf7
Tomas Rodriguez <trodriguez@xebia.fr>
2016-03-01 15:15:12 +0100

    Exercice 1 step 3

--- src/main/scala/fr/xebia/xke/fp5/exercise1/Average.scala
+++ src/main/scala/fr/xebia/xke/fp5/exercise1/Average.scala
@@ -14,7 +14,7 @@ object OopAverage {
   /*
   Implémenter une fonction qui calcule l'average d'une liste d'entiers
    */
-  def avgDouble(numbers: Double*): Double = ???
+  def avgDouble(numbers: Double*): Double = numbers.reduce(_ + _) / numbers.length
 
 
   /**
@@ -22,6 +22,30 @@ object OopAverage {
     *
     * Q: Peut-on faire une fonction générique qui traite les Int et les Double à la fois? Dit
     * autrement, peut-on mutualiser le code des deux fonctions?
+    * R: Oui, on peut, en utilisant par example le pattern Wrapper/Adapter
     *
     */
+  sealed trait NumberAdapter {
+    def +(n: NumberAdapter): NumberAdapter
+
+    def /(n: NumberAdapter): NumberAdapter
+  }
+
+  def avgNumber(numbers: NumberAdapter*): NumberAdapter = ??? /*
+                                                              YOUR CODE HERE
+                                                               */
+
+  /**
+    * Example d'utilisation de avgNumber pour une liste de Int
+    */
+  lazy val avgIntNumber = avgNumber(IntNumberAdapter(5), IntNumberAdapter(4), IntNumberAdapter(3), IntNumberAdapter(2), IntNumberAdapter(1))
+
+  /**
+    * Example d'utilisation de avgNumber pour une liste de Double
+    */
+  lazy val avgDoubleNumber = avgNumber(DoubleNumberAdapter(5.0), DoubleNumberAdapter(4.0), DoubleNumberAdapter(3.0), DoubleNumberAdapter(2.0), DoubleNumberAdapter(1.0))
+
+  /*
+  YOUR CODE HERE
+   */
 }
